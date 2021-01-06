d2aec10d8d68c70348ffb4e9dbd8ae6ec92f1cf7
David Konsumer <konsumer@jetboystudio.com>
2021-01-06 15:58:05 -0800

    got string ffi working

--- src/rattata.lua
+++ src/rattata.lua
@@ -1,18 +1,20 @@
 local ffi = require("ffi")
 
-local rattata = {}
+local f_rattata = ffi.load("rattata")
 
 ffi.cdef[[
   const int8_t *rattata_location(void);
   const int8_t *rattata_hostname(void);
 ]]
 
+local rattata = {}
+
 function rattata:hostname()
-  return ffi.C.rattata_hostname()
+  return ffi.string(f_rattata.rattata_hostname())
 end
 
 function rattata:location()
-  return ffi.C.rattata_location()
+  return ffi.string(f_rattata.rattata_location())
 end
 
 return rattata
