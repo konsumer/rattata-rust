76a7c2a19470ce4027aacd665a0fb26f25db5cfd
David Konsumer <konsumer@jetboystudio.com>
2021-01-05 21:50:28 -0800

    tidy parts into little functions

--- src/manager/main.rs
+++ src/manager/main.rs
@@ -1,31 +1,44 @@
-use libtor::{ Tor, TorFlag, HiddenServiceVersion, TorAddress, log };
+use libtor::{ Tor, TorFlag, HiddenServiceVersion, TorAddress, Error, log };
 use dirs;
-use std::fs::File;
-use std::io::Read;
+use std::fs::{ File };
+use std::io::{ Read };
+use std::thread::{ JoinHandle, sleep };
+use std::time::{ Duration };
 
-fn main() {
-    let config_dir = dirs::config_dir().unwrap().into_os_string().into_string().unwrap();
-    
-    let server = Tor::new()
+/// get the current setings dir (which has tor stuff in it)
+fn rattata_location() -> String {
+    return format!("{}/rattata", dirs::config_dir().unwrap().into_os_string().into_string().unwrap());
+}
+
+/// start a tor server & the local service connected to it
+fn rattata_start(port: u16) -> JoinHandle<Result<u8, Error>> {
+    // TODO: write actual service here
+
+    return Tor::new()
         .flag(TorFlag::SocksPort(0))
-        .flag(TorFlag::HiddenServiceDir(format!("{}/rattata", config_dir)))
+        .flag(TorFlag::HiddenServiceDir(rattata_location()))
         .flag(TorFlag::HiddenServiceVersion(HiddenServiceVersion::V3))
-        .flag(TorFlag::HiddenServicePort(TorAddress::Port(8000), None.into()))
+        .flag(TorFlag::HiddenServicePort(TorAddress::Port(port), None.into()))
         .flag(TorFlag::Log(log::LogLevel::Err))
         .start_background();
-    
-    // TODO: write actual service here
-    
-    match File::open(format!("{}/rattata/hostname", config_dir)) {
-        Ok(mut file) => {
-            let mut content = String::new();
-            file.read_to_string(&mut content).unwrap();
-            println!("Server running at {}", content);
-            let _ = server.join();
-        },
-        
-        Err(error) => {
-            println!("Error opening host file: {}", error);
-        }
-    }
+}
+
+/// get the current onion-hostname from running tor-server
+fn rattata_hostname() -> String {
+    let mut content = String::new();
+    let fname = format!("{}/hostname", rattata_location());
+    let f = File::open(&fname);
+    let _ = match f {
+        Ok(mut file) => file.read_to_string(&mut content),
+        Err(error) => panic!("Problem opening the file {}: {:?}", &fname, error),
+    };
+    return content;
+}
+
+fn main() {
+    let server = rattata_start(8000);
+    sleep(Duration::from_millis(1000));
+    let hostname = rattata_hostname();
+    println!("Server running at {}", &hostname);
+    let _ = server.join();
 }
