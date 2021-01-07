b66f0f4ee7c9fc9908c16604ce85b25a41ebca38
David Konsumer <konsumer@jetboystudio.com>
2021-01-07 01:07:19 -0800

    put hostname on same line, for easier disabling

--- src/manager/main.rs
+++ src/manager/main.rs
@@ -6,7 +6,6 @@ fn main() {
     let server = spawn(|| rattata::start(8000));
     sleep(Duration::from_millis(2000));
     println!("Files in {}", rattata::location());
-    let hostname = rattata::hostname();
-    println!("Server running at {}", hostname);
+    println!("Server running at {}", rattata::hostname());
     let _ = server.join();
 }
